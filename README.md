a598c201adae0ced71d6cae2edb98efea75006d7
Peter Inglesby <peter.inglesby@gmail.com>
2020-09-02 11:38:46 +0100

    Add study definition and codelists to generate SQL for acceptance test

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-# OpenSAFELY Research Template
+# EMIS acceptance test
 
-This is a template repository for making new OpenSAFELY resarch projects.  Eventually it'll become a framework. To get started, create a new repo using this repo as a template, delete this front matter, and edit the text that follows.
+This repo contains a study definition to generate SQL to test that the EMIS
+database schema is as we expect.
 
-# _title goes here_
+The study definition is not clinically meaningful, but exercises most of the
+cohort-generator code that generates SQL.
 
-This is the code and configuration for our paper, _name goes here_
+To run:
 
-* The paper is [here]()
-* Raw model outputs, including charts, crosstabs, etc, are in `released_analysis_results/`
-* If you are interested in how we defined our variables, take a look at the [study definition](analysis/study_definition.py); this is written in `python`, but non-programmers should be able to understand what is going on there
-* If you are interested in how we defined our code lists, look in the [codelists folder](./codelists/).
-* Developers and epidemiologists interested in the code should review
-[DEVELOPERS.md](./docs/DEVELOPERS.md).
+    DATABASE_URL=presto:// EMIS_ORGANISATION_HASH=<EMIS_ORGANISATION_HASH> cohortextractor dump_cohort_sql --study-definition analysis.study_definition
 
 # About the OpenSAFELY framework
 
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,63 +1,84 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv
+from cohortextractor import (
+    StudyDefinition,
+    patients,
+    codelist_from_csv,
+    codelist,
+)
 
-chronic_cardiac_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID"
+
+covid_codelist = codelist(["U071", "U072"], system="icd10")
+
+asthma_codes = codelist_from_csv(
+    "codelists/opensafely-asthma-diagnosis-snomed.csv", system="snomedct", column="id"
 )
-chronic_liver_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-liver-disease.csv", system="ctv3", column="CTV3ID"
-)
-salbutamol_codes = codelist_from_csv(
-    "codelists/opensafely-asthma-inhaler-salbutamol-medication.csv",
-    system="snomed",
+
+chronic_respiratory_disease_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-respiratory-disease-snomed.csv",
+    system="snomedct",
     column="id",
 )
-systolic_blood_pressure_codes = codelist(["2469."], system="ctv3")
-diastolic_blood_pressure_codes = codelist(["246A."], system="ctv3")
+
+pred_codes = codelist_from_csv(
+    "codelists/opensafely-asthma-oral-prednisolone-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+
+
+systolic_blood_pressure_codes = codelist(["163030003"], system="snomedct")
+diastolic_blood_pressure_codes = codelist(["163031004"], system="snomedct")
+
+## STUDY POPULATION
+# Defines both the study population and points to the important covariates
 
 study = StudyDefinition(
-    # Configure the expectations framework
-    default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
-        "rate": "exponential_increase",
-    },
     # This line defines the study population
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
+    # Outcomes
+    icu_date_admitted=patients.admitted_to_icu(
+        on_or_after="2020-02-01",
+        include_day=True,
+        returning="date_admitted",
+        find_first_match_in_period=True,
+    ),
+    died_date_cpns=patients.with_death_recorded_in_cpns(
+        on_or_before="2020-06-01",
+        returning="date_of_death",
+        include_month=True,
+        include_day=True,
+    ),
+    died_ons_covid_flag_any=patients.with_these_codes_on_death_certificate(
+        covid_codelist, on_or_before="2020-06-01", match_only_underlying_cause=False
+    ),
+    died_ons_covid_flag_underlying=patients.with_these_codes_on_death_certificate(
+        covid_codelist, on_or_before="2020-06-01", match_only_underlying_cause=True
+    ),
+    died_date_ons=patients.died_from_any_cause(
+        on_or_before="2020-06-01",
+        returning="date_of_death",
+        include_month=True,
+        include_day=True,
+    ),
     # The rest of the lines define the covariates with associated GitHub issues
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/33
-    age=patients.age_as_of(
-        "2020-02-01",
-        return_expectations={
-            "rate": "universal",
-            "int": {"distribution": "population_ages"},
-        },
-    ),
+    age=patients.age_as_of("2020-02-01"),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/46
-    sex=patients.sex(
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"M": 0.49, "F": 0.51}},
-        }
+    sex=patients.sex(),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/52
+    imd=patients.address_as_of(
+        "today", returning="index_of_multiple_deprivation", round_to_nearest=100
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/7
-    chronic_cardiac_disease=patients.with_these_clinical_events(
-        chronic_cardiac_disease_codes,
-        returning="date",
-        find_first_match_in_period=True,
-        include_month=True,
-        return_expectations={"incidence": 0.2},
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/37
+    rural_urban=patients.address_as_of(
+        "today", returning="rural_urban_classification"
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/12
-    chronic_liver_disease=patients.with_these_clinical_events(
-        chronic_liver_disease_codes,
-        returning="date",
-        find_first_match_in_period=True,
-        include_month=True,
-        return_expectations={
-            "incidence": 0.2,
-            "date": {"earliest": "1950-01-01", "latest": "today"},
-        },
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/54
+    stp=patients.registered_practice_as_of("2020-02-01", returning="stp_code"),
+    # region - one of NHS England 9 regions
+    region=patients.registered_practice_as_of(
+        "2020-02-01", returning="nuts1_region_name"
     ),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/10
     bmi=patients.most_recent_bmi(
@@ -65,11 +86,48 @@ study = StudyDefinition(
         minimum_age_at_measurement=16,
         include_measurement_date=True,
         include_month=True,
-        return_expectations={
-            "incidence": 0.6,
-            "float": {"distribution": "normal", "mean": 35, "stddev": 10},
-        },
     ),
+    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/55
+    asthma=patients.categorised_as(
+        {
+            "0": "DEFAULT",
+            "1": """
+                (
+                  recent_asthma_code OR (
+                    asthma_code_ever AND NOT
+                    copd_code_ever
+                  )
+                ) AND (
+                  prednisolone_last_year = 0 OR 
+                  prednisolone_last_year > 4
+                )
+            """,
+            "2": """
+                (
+                  recent_asthma_code OR (
+                    asthma_code_ever AND NOT
+                    copd_code_ever
+                  )
+                ) AND
+                prednisolone_last_year > 0 AND
+                prednisolone_last_year < 5
+                
+            """,
+        },
+        recent_asthma_code=patients.with_these_clinical_events(
+            asthma_codes, between=["2017-02-01", "2020-02-01"],
+        ),
+        asthma_code_ever=patients.with_these_clinical_events(asthma_codes),
+        copd_code_ever=patients.with_these_clinical_events(
+            chronic_respiratory_disease_codes
+        ),
+        prednisolone_last_year=patients.with_these_medications(
+            pred_codes,
+            between=["2019-02-01", "2020-02-01"],
+            returning="number_of_matches_in_period",
+        ),
+    ),
+    # Blood pressure
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/35
     bp_sys=patients.mean_recorded_value(
         systolic_blood_pressure_codes,
@@ -77,10 +135,6 @@ study = StudyDefinition(
         on_or_before="2020-02-01",
         include_measurement_date=True,
         include_month=True,
-        return_expectations={
-            "incidence": 0.6,
-            "float": {"distribution": "normal", "mean": 80, "stddev": 10},
-        },
     ),
     bp_dias=patients.mean_recorded_value(
         diastolic_blood_pressure_codes,
@@ -88,53 +142,5 @@ study = StudyDefinition(
         on_or_before="2020-02-01",
         include_measurement_date=True,
         include_month=True,
-        return_expectations={
-            "incidence": 0.6,
-            "float": {"distribution": "normal", "mean": 120, "stddev": 10},
-        },
-    ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/54
-    stp=patients.registered_practice_as_of(
-        "2020-02-01",
-        returning="stp_code",
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"STP1": 0.5, "STP2": 0.5}},
-        },
-    ),
-    msoa=patients.registered_practice_as_of(
-        "2020-02-01",
-        returning="msoa_code",
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"MSOA1": 0.5, "MSOA2": 0.5}},
-        },
-    ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/52
-    imd=patients.address_as_of(
-        "2020-02-01",
-        returning="index_of_multiple_deprivation",
-        round_to_nearest=100,
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
-        },
-    ),
-    rural_urban=patients.address_as_of(
-        "2020-02-01",
-        returning="rural_urban_classification",
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"rural": 0.1, "urban": 0.9}},
-        },
-    ),
-    recent_salbutamol_count=patients.with_these_medications(
-        salbutamol_codes,
-        between=["2018-02-01", "2020-02-01"],
-        returning="number_of_matches_in_period",
-        return_expectations={
-            "incidence": 0.6,
-            "int": {"distribution": "normal", "mean": 8, "stddev": 2},
-        },
     ),
 )
